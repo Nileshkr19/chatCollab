9228f1dd66d197116271c3daf30eb82bd7c74cc4
Nileshkr19 <nileshsbg191914@gmail.com>
2026-03-20 22:19:45 +0530

    Add user model and utility classes for error handling and responses

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,10 @@
+class apiResponse {
+  constructor(statusCode, data, message = "success") {
+    this.statusCode = statusCode;
+    this.message = message;
+    this.data = data;
+    this.success = statusCode >= 200 && statusCode < 300;
+  }
+}
+
+export default apiResponse;
